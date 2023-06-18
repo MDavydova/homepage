0ecab1647ee19909377ebdcf951e71ba9f46c3ee
Milena Davydova <milena@yelk.com.ua>
2023-06-18 19:11:45 +0200

    feat: completed markup and styles of contact section

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-document.addEventListener("DOMContentLoaded", () => {});
+function initCopyContactLink() {
+  // Get the text field
+  const display = document.getElementById("contactDisplay");
+
+  const contactLinks = document.querySelectorAll(".contact button");
+
+  const hiddenInput = document.getElementById("contactDisplayInput");
+
+  contactLinks.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      display.textContent = btn.dataset.value;
+      hiddenInput.value = btn.dataset.value;
+    });
+  });
+
+  display.addEventListener("click", () => {
+    navigator.clipboard.writeText(hiddenInput.value).then(function (x) {
+      alert("Link copied to clipboard: " + hiddenInput.value);
+    });
+  });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  initCopyContactLink();
+});
